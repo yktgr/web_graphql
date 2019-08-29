4be439a5ef4b0d83f1f5bf462fc6ba55059d66c6
user <user@usernoMacBook-Pro.local>
2019-08-29 21:30:39 +0900

    init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,30 +1,9 @@
+require 'fileutils'
+preload_app true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
 
-rails_root = File.expand_path('../../', __FILE__)
-
-worker_processes 2
-working_directory rails_root
-
-listen "#{rails_root}/tmp/sockets/unicorn.sock"
-pid "#{rails_root}/tmp/pids/unicorn.pid"
-
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
 end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
